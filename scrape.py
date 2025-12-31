435ace026f9ac3d7b5d0a27792066b01b7d1932d
krishna khadka06 <rdk848090@gmail.com>
2025-12-31 14:32:15 +0545

    new commends

--- scrape.py
+++ scrape.py
@@ -11,6 +11,13 @@
 #always
 #git add .
 #git commit-m "your message"
+
+
+# => we usually do
+# 1 change the code 
+# 2 git add .
+# 3 git commit -m "message"
+# 4 git push
 import json
 import csv
 import requests
